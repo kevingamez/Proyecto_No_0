4a533fd7152bee84358283472dec03aa7de1d792
Kevin Gamez <ks.gamez@uniandes.edu.co>
2024-02-11 03:59:32 -0500

    Object serialization

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from backend import create_app
-from .models import db, Task
+from .models import *
+from datetime import datetime, timedelta  
 
 app = create_app('default')
 app_context = app.app_context()
@@ -7,3 +8,21 @@ app_context.push()
 
 db.init_app(app)
 db.create_all()
+
+with app.app_context():
+    t = Task(
+    text="Finish the report",
+    deadline=datetime.utcnow() + timedelta(days=7), 
+    status=StatusEnum.NOT_STARTED, 
+    user=1,
+)
+    t2 =  Task(
+    text="Finishasd the report",
+    deadline=datetime.utcnow() + timedelta(days=7), 
+    status=StatusEnum.NOT_STARTED, 
+    user=1,
+)
+    db.session.add(t)
+    db.session.add(t2)
+    db.session.commit()
+    print([TaskSchema().dump(task) for task in Task.query.all()])
--- backend/models/models.py
+++ backend/models/models.py
@@ -1,8 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
 
 db = SQLAlchemy()
 
-class StatusEnum(db.Enum):
+class StatusEnum(Enum):
     NOT_STARTED = 'Not Started'
     IN_PROGRESS = 'In Progress'
     COMPLETED = 'Completed'
@@ -11,14 +14,14 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(128), nullable=False)
     creationDate = db.Column(db.DateTime, default=db.func.current_timestamp())
-    deadline = db.Column(db.Boolean, default=False)
-    status = db.Column(db.Enum(StatusEnum), default=StatusEnum.NOT_STARTED)
+    deadline = db.Column(db.DateTime, default=False) 
+    status = db.Column(db.Enum(StatusEnum))
     user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    Category = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    Category = db.Column(db.Integer, db.ForeignKey('category.id'))
     __table_args__ = (db.UniqueConstraint('text', 'user', name='unique_task'),)
 
     def __repr__(self):
-        return f'<Task {self.title}>'
+        return f'<Task {self.text}>'
     
     
 class Category(db.Model):
@@ -41,4 +44,19 @@ class User(db.Model):
     taskList = db.relationship('Task',cascade='all, delete, delete-orphan')
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.name}>'
+    
+    
+class EnumToDict(fields.Field):
+    def _serialize(self, value, attr, obj, **kwargs):
+        if value is None:
+            return None
+        print(value)
+        return {'key': value.name, 'value': value.value}
+    
+class TaskSchema(SQLAlchemyAutoSchema):
+    status = EnumToDict(attribute=("status"))
+    class Meta:
+        model = Task
+        include_relationships = True
+        load_instance = True
